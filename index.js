ae29738a12d6114c8e183279d32d167d67516ac1
Khushi740 <99756558+Piyush9140@users.noreply.github.com>
2024-03-28 02:20:53 +0530

    Tweak:Index file

--- index.js
+++ index.js
@@ -5,7 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
-import Loginscreen from './src/screens/Loginscreen';
-import ImageCarousel from './src/components/ImageCarousel';
 
-AppRegistry.registerComponent(appName, () => ImageCarousel);
+AppRegistry.registerComponent(appName, () => App);
