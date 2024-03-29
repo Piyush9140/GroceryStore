72e45360cf0d0c9fd18d5fcbfa63cfe33745fd80
Khushi740 <99756558+Piyush9140@users.noreply.github.com>
2024-03-29 18:57:31 +0530

    feat:Product-Card Component Added

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, ToastAndroid } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
-const ProductCard = ({ image, title, price, onPress, Quantity ,Discountprice}) => {
+const ProductCard = ({ image, Title, Price, onPress, Quantity }) => {
   return (
-    <View style={styles.container}>
-      <Image source={image} style={styles.image} />
-      <Text style={styles.title}>{title}</Text>
+    <View style={styles.Container}>
+      <Image source={image} style={styles.Image} />
+      <Text style={styles.Title}>{Title}</Text>
       <Text>{Quantity}</Text>
       <View style={{flexDirection:"row",justifyContent:'space-between',alignItems:"center"}}>
      <View>
-      <Text style={styles.price}>₹{price}</Text>
-      <Text style={styles.Disprice}>₹{Discountprice}</Text>
-      <View style={styles.cutLine} />
+      <Text style={styles.Price}>₹{Price}</Text>
      </View>
-      <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text style={styles.buttonText}>ADD</Text>
+      <TouchableOpacity style={styles.Button} onPress={onPress}>
+        <Text style={styles.ButtonText}>ADD</Text>
       </TouchableOpacity>
       </View>
       
@@ -23,53 +21,33 @@ const ProductCard = ({ image, title, price, onPress, Quantity ,Discountprice}) =
 };
 
 const styles = StyleSheet.create({
-  container: {
+  Container: {
     width: 150,
     margin:5
   },
-  image: {
+  Image: {
     width: '100%',
     height: 150,
   },
-  title: {
+  Title: {
     fontSize: 18,
     color:'black'
   },
-  price: {
+  Price: {
     fontSize: 16,
     color:'black'
  },
- Disprice:{
-  fontSize: 16,
- },
- cutLine: {
-  position: 'absolute',
-  top: '75%',
-  left: 0,
-  right: 0,
-  height: 1,
-  backgroundColor: '#6d6c6c',
-  zIndex: 1, 
-},
-  button: {
+  Button: {
     borderRadius: 10,
     paddingVertical: 5,
     paddingHorizontal: 20,
     borderWidth:1,
     borderColor:'#F97316',
   },
-  buttonText: {
+  ButtonText: {
     color: '#F97316',
     fontSize: 16,
   },
 });
 
 export default ProductCard;
-// Example to use 
-//  <ProductCard
-// image=
-// {{
-//   uri: 'https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/app/assets/products/sliding_images/jpeg/7a97253f-581e-4c0a-b48e-37e7a7066300.jpg?ts=1708589141',
-// }}
-// title="Product Title" price="XX" Quantity='1Kg' Discountprice='100' 
-// />
